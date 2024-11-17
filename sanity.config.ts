582064946daf99ae7c2af1fb52128646881e287b
dbutch25 <danielbutcher25@gmail.com>
2024-11-16 22:41:49 -0500

    refractor components and props add backend to service pages

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,12 +6,12 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {structureTool} from '@/sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
-import {structure} from './sanity/structure'
+import {apiVersion, dataset, projectId} from './env'
+import {schema} from './schema'
+import {structure} from './structure'
 
 export default defineConfig({
   basePath: '/studio',
