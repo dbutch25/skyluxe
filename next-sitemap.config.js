d025dcfe356ecf21005e065b6de7814bd92358cc
dbutch25 <danielbutcher25@gmail.com>
2024-11-26 03:05:07 -0500

    sitemap & robots.txt file added

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "skyluxeroofing.com",
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 1,
+    sitemapSize: 7000,
+}
