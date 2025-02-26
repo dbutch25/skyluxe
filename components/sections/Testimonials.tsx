60d69f8e8146ea90b48511db678e567501f38770
dbutch25 <danielbutcher25@gmail.com>
2025-02-25 21:53:58 -0500

    added images to testimonial adjusted design

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -10,13 +10,14 @@ const Testimonials = () => {
             <span className="h-1 w-6 bg-secondary-600 block rounded-full"/>
             <span>From Our Clients</span>
         </div>
-        <div className="rounded-md flex flex-col antialiased bg-primary-50 dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <div className="rounded-md flex flex-col antialiased bg-primary-50 items-center justify-center relative overflow-hidden">
         <InfiniteMovingCards
             items={testimonials}
             direction="right"
             speed="slow"
         />
         </div>
+        
     </div>
   );
 }
@@ -26,35 +27,43 @@ const testimonials = [
     quote:
       "The BEST in the business - I can not thank Mark and his team at Skyluxe enough for their amazing work, attention to detail and professionalism.  From roofing, to ACM to a floating deck, these guys can really do it all!  I would highly recommend their services, and the photos speak for themselves.",
     name: "Ontario Dental Implant Network",
+    img: "https://lh3.googleusercontent.com/a/ACg8ocIX6Bdp8GQ9BG8qTfiCi-v5At_LycB4_otkZ5LOICKd62ax-g=w65-h65-p-rp-mo-br100"
   },
   {
     quote:
       "Skyluxe did my modern homes' flat roof in Oakville on Chartwell Rd, they have a great crew and do very clean work! They fixed the slopping issue and updated the drains, so no more standing wate like before I would highly recommend them if you are doing a higher end project and are particular of the fit and finnish",
     name: "Daniel - PinkCherry",
+    img: 'https://lh3.googleusercontent.com/a-/ALV-UjXVPLE6mIHySZD3Q3x3ThogW3mOiUk5azlQYWKJSC6CbBrb6Uyb=w65-h65-p-rp-mo-br100'
   },
   {
     quote: "I have used Skyluxe Roofing 3 times - a large complicated flat roof on my studio/living space building needed replacing, they sorted out a leaking roof on my sister's home and finally I had the company redo my flat garage roof. Mark and his crew were attentive to what I needed and extremely tidy. Mark is up on all of the latest roofing materials and because of their workmanship and dependability...",
     name: "william roddy",
+    img: "https://lh3.googleusercontent.com/a/ACg8ocJexNkymcsAwdY74UyL5AEG2KC0SY2uy_KopTjDAlWb3UvbOQ=w65-h65-p-rp-mo-br100"
   },
   {
     quote:
       "Skyluxe is an amazing overall outfit for your custom home or commercial project.  I used them for my roof along with ACM for my residential home.  They were quick, reliable, and extremely professional.  Their crew was fast and efficient leaving my jobsite very clean after they were complete.",
     name: "Courtney Borg",
+    img: "https://lh3.googleusercontent.com/a/ACg8ocITDEJjOj2k4jlTS9SaQjw3V9bNdIRDpmI4i6KNLlImS2exzw=w65-h65-p-rp-mo-br100"
   },
   {
     quote:
       "A Truly one of kind company. Unmatched when it comes to quality, professionalism, and transparency. Wouldn’t trust any roofing or waterproofing company other than Skyluxe. Highly recommended and a top tier company from the bottom up. Mark and his entire team are second to none. Thank you for your services.",
     name: "K F",
+    img: "https://lh3.googleusercontent.com/a/ACg8ocLW7HH3y9IVzeVDFYGI-yGIaMMrY0vOI3eHL_Gz7FspjZA3mA=w65-h65-p-rp-mo-br100"
   },
   {
     quote:
       "If you’re looking for roofing services or ACM panel/metal cladding work, this is hands down the best company in Ontario. They excel in handling everything from residential to commercial projects with unparalleled expertise and professionalism. The entire team is exceptionally knowledgeable...",
     name: "Samantha Katouzian",
+    img: "https://lh3.googleusercontent.com/a-/ALV-UjUclQ5Lcooym1DAcR4rEOCwJDXMhT9Xj8PmIcaw4f8A_WLyNuY=w65-h65-p-rp-mo-br100"
+
   },
   {
     quote:
       "The customer service was exceptional, and the installation team demonstrated utmost professionalism, organization, and cleanliness. Their efforts resulted in a remarkable transformation of my old house, making it appear brand new. I would like to express my sincere gratitude...",
     name: "israel izzy",
+    img: "https://lh3.googleusercontent.com/a-/ALV-UjWD2fJy2uOHkchGHM4MDgbmGXMTUgNL8yctNarvwzwVOKKiCtXz=w65-h65-p-rp-mo-br100"
   },
 ];
 
--- components/ui/MovingCards.tsx
+++ components/ui/MovingCards.tsx
@@ -6,13 +6,14 @@ import React, { useEffect, useState } from "react";
 export const InfiniteMovingCards = ({
   items,
   direction = "left",
-  speed = "fast",
-  pauseOnHover = true,
+  speed = "slow",
+    pauseOnHover = true,
   className,
 }: {
   items: {
     quote: string;
     name: string;
+    img: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -60,11 +61,11 @@ export const InfiniteMovingCards = ({
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s");
+        containerRef.current.style.setProperty("--animation-duration", "60s");
       } else if (speed === "normal") {
-        containerRef.current.style.setProperty("--animation-duration", "40s");
-      } else {
         containerRef.current.style.setProperty("--animation-duration", "80s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "100s");
       }
     }
   };
@@ -72,21 +73,21 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-10  w-full mx-auto overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-10 w-screen overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
       <ul
         ref={scrollerRef}
         className={cn(
-          " flex min-w-full shrink-0 gap-20 py-4 w-max flex-nowrap",
+          " flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap",
           start && "animate-scroll ",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 bg-[rgb(255,253,245)] border-secondary-700 px-8 py-6 md:w-[450px]"
+            className="w-[90vw] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-800 p-5 md:p-16 md:w-[60vw]"
             key={idx}
           >
             <blockquote>
@@ -94,12 +95,15 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 leading-[1.6] text-primary-950 font-montserrat text-sm">
+              <span className=" relative z-10 leading-[1.6] text-primary-950 font-montserrat text-sm md:text-lg">
                 {item.quote}
               </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
+              <div className="relative z-10 mt-6 flex flex-row items-center">
+                <div className="me-3">
+                  <img src={item.img} alt={item.name} />
+                </div>
                 <span className="flex flex-col gap-1">
-                  <span className=" leading-[1.6] text-secondary-800 font-julius text-base">
+                  <span className=" leading-[1.6] text-secondary-800 font-julius text-lg md:text-xl">
                     {item.name}
                   </span>
                 </span>
